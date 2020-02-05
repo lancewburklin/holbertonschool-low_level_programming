4dfb18c5cb1c83ece3bcf4fc54143bb6471720b8
lancewburklin <1677@holbertonschool.com>
2020-02-05 20:43:25 +0000

    Made Betty happy

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/**This is
+/*
+*This is
 *a comment
 */
 
-int main(void){
+int main(void)
+{
 	puts("\"Programming is like building a multilingual puzzle");
-	     return 0;
+	return (0);
 }
