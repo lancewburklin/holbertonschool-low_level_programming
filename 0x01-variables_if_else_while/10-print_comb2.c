e6d7fc11a44bcd05ca392bc3b303d45cd2b56a27
lancewburklin <1677@holbertonschool.com>
2020-02-06 22:26:30 +0000

    Count 1 to 99

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - print plenty of numbers
+ *
+ * Return: ALways 0
+ */
+
+int main(void)
+{
+	int o;
+	int p;
+
+	for (o = 48; o < 58; o++)
+	{
+		for (p = 48; p < 58; p++)
+		{
+			putchar(o);
+			putchar(p);
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar(10)
+		return (0)
+}
