5d7aec45a94d017906c3e30831498bc51a1964f0
lancewburklin <1677@holbertonschool.com>
2020-02-06 20:32:26 +0000

    Print the alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int lett;
+		for (lett = 97; lett <= 122; lett++)
+			putchar (lett);
+	putchar (10);
+		return (0);
+}
