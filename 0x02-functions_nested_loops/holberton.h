d1cbce17c4739af0dfb7607626cae23c7dbb6d72
lancewburklin <1677@holbertonschool.com>
2020-02-10 21:13:04 +0000

    Adds new prototype

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -6,6 +6,7 @@
  * @c: Just an input
  * print_alphabet - Prints the alphabet
  * print_alphabet_x10 - Print the alphabet 10 times
+ * _islower - Is it lowercase or nah
  *
  * Return: 0, of course
  */
@@ -15,5 +16,7 @@ int _putchar(char c);
 void print_alphabet(void);
 
 void print_alphabet_x10(void);
+
+int _islower(int c);
 
 #endif
