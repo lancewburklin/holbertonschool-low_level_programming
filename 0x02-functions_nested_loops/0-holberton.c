6779f44f446f7f3a3c809725827983771bd35d6e
lancewburklin <1677@holbertonschool.com>
2020-02-10 18:39:34 +0000

    Fixed to holberton.h

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,13 +1,22 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - print Holberton
+ * main - print a line of text
  *
- * Return: Always 0 broski
+ * Return: 0
  */
 
 int main(void)
 {
-	printf("Holberton\n");
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+		_putchar('o');
+	_putchar('n');
+	_putchar(10);
 	return (0);
 }
