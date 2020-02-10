2b49277def5dcc540f1f2c97daab5d1f4bde1b8b
lancewburklin <1677@holbertonschool.com>
2020-02-10 23:46:32 +0000

    Returns absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "holberton.c"
+
+/**
+ * _abs - Print absolute value
+ * @x: This is any value
+ *
+ * Return: 0
+ */
+
+int _abs(int x)
+{
+	if (x < 0)
+		return (-x);
+	else if (x >= 0)
+		return (x);
+}
