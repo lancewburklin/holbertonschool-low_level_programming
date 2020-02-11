7c4de2a234161cf5108ecce482fff3b3b504fb09
lancewburklin <1677@holbertonschool.com>
2020-02-11 20:13:45 +0000

    Counts to 98)

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints to 98
+ * @n: Any natural interger
+ *
+ * Return: Those ugly socks
+ */
+
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		for (n = n; n <= 98; n++)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(" ,");
+		}
+	}
+	else
+	{
+		for (n = n; n >= 98; n--)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(" ,");
+		}
+	}
+}
