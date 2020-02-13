5bf44bb14833bae5bb984cd9dc291db555ecd87b
lancewburklin <1677@holbertonschool.com>
2020-02-13 19:50:49 +0000

    Removed final space

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i;
 
-	for (i = 1; i < 101; i++)
+	for (i = 1; i < 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz ");
@@ -21,6 +21,8 @@ int main(void)
 		else
 			printf("%d ", i);
 	}
+	if (i == 100)
+		printf("Buzz");
 	putchar(10);
 	return (0);
 }
