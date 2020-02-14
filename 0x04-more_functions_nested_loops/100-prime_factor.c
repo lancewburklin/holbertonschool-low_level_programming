a1cc93f8c47949cc37256b955869b2938b629309
lancewburklin <1677@holbertonschool.com>
2020-02-14 19:33:25 +0000

    Fixed program

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -12,20 +12,12 @@ int main(void)
 	unsigned long factor;
 	unsigned long storage;
 
-	for (factor = 2; factor < 204284158382; factor++)
+	for (factor = 2; factor < num; factor++)
 	{
-		if (num % factor == 0)
+		while (num % factor == 0)
 		{
-			storage = factor;
-			printf("%lu", storage);
-			if (factor == 3)
-			{
-				factor = factor * 1300;
-			}
-			else if (factor == 4019)
-				factor = factor * 12645;
-			else if (factor == 50829599)
-				factor = factor * 4;
+			num = num / factor;
+			storage = num;
 		}
 	}
 	printf("%lu\n", storage);
