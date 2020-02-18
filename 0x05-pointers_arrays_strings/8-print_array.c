b77d3afcf5f81cf4494b8855269508cf953f1cc3
lancewburklin <1677@holbertonschool.com>
2020-02-18 16:25:35 +0000

    Changed comma space

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,7 +12,8 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
-		printf("%d, ", a[i]);
-	printf("%d\n", a[n - 1]);
+	printf("%d", a[0]);
+	for (i = 1; i < n; i++)
+		printf(", %d", a[i]);
+	printf("\n");
 }
