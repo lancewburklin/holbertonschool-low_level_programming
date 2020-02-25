9018df653fcdde316df6c81b0ea0df030a7651e3
lancewburklin <1677@holbertonschool.com>
2020-02-25 04:26:24 +0000

    Sets a string

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * set_string - Sets a string
+ * @s: Original string
+ * @to: New string
+ *
+ * Return: void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
