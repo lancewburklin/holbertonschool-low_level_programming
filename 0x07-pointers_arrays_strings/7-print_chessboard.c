c4bd648427e5c6a4da5089ac44d9c6fdf799bd09
lancewburklin <1677@holbertonschool.com>
2020-02-25 03:35:07 +0000

    Prints a chess board

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_chessboard - Prints a chessboard
+ * @a: Board
+ * @8: Size
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i;
+	int n;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar(10);
+	}
+}
