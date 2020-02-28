053d9ae832aca98abe0611a8afbc561d52551489
lancewburklin <1677@holbertonschool.com>
2020-02-28 19:07:38 +0000

    Prints file name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - The main function
+ * @argc - Argument count
+ * @argv - Array of argument strings
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
