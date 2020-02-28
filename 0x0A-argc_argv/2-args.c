087e4b0c7529fddd200b6318bcca50bcbc47f71a
lancewburklin <1677@holbertonschool.com>
2020-02-28 19:27:27 +0000

    This will print all arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Prints arguments
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: Absolutely nothing
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
