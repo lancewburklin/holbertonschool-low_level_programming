544aaf2050a2dc3fa57f72270874cad5bfcf15b3
lancewburklin <1677@holbertonschool.com>
2020-03-01 05:08:45 +0000

    Check to see if any numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,12 +13,26 @@
 int main(int argc, char *argv[])
 {
 	int sum = 0;
-	int i, j;
+	int i, j, n, c;
+	int flag = 0;
 
 	if (argc == 1)
 	{
 		printf("%d\n", 0);
 		return (1);
+	}
+	for (n = 1; n < argc; n++)
+	{
+		for (c = 0; argv[n][c] != '\0'; c++)
+		{
+			if (isdigit(argv[n][c]))
+				flag = 1;
+		}
+		if (flag == 0)
+		{
+			printf("%d\n", 0);
+			return (1);
+		}
 	}
 	for (i = 1; i < argc; i++)
 	{
