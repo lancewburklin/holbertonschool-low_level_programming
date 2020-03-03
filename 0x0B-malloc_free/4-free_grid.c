4b72d1b6d15ace685b12bd8af58f24dee549c220
lancewburklin <1677@holbertonschool.com>
2020-03-03 21:31:31 +0000

    Sets info free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a grid
+ * @grid: The grid
+ * @height: Height of the grid
+ *
+ * Return: Nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
