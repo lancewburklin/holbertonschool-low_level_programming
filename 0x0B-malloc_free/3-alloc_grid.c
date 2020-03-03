e1889c96ed0ca4d79a3bb039bbc776e0a3322187
lancewburklin <1677@holbertonschool.com>
2020-03-03 19:27:20 +0000

    Prints a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * alloc_grid - Makes a two dimensional array
+ * @width: Width of the array
+ * @height: Height of the array
+ *
+ * Return: The grid or null
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **newarray;
+	int i, n, c;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+	newarray = malloc(width * sizeof(int*));
+	if (newarray == NULL)
+		return (NULL);
+	for (c = 0; c < width; c++)
+	{
+		newarray[c] = malloc(height * sizeof(int));
+	}
+	for (i = 0; i < height; i++)
+	{
+		for (n = 0; n < width; n++)
+		{
+			newarray[i][n] = 0;
+		}
+	}
+	return (newarray);
+}
