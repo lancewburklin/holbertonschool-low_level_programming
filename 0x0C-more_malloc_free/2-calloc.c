0ae95b78d9ec6ccd103ce2ca28ab352572d4a190
lancewburklin <1677@holbertonschool.com>
2020-03-05 20:13:34 +0000

    Fixed malloc position

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,9 +15,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ar;
 
-	ar = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
+	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb * size; i++)
