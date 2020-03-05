73b1a880805ec298cbf729933124410616ce24f6
lancewburklin <1677@holbertonschool.com>
2020-03-05 20:09:09 +0000

    Fixed some errors

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,12 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ar;
 
+	ar = malloc(nmemb * size);
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		ar[i] = 0;
 	return (ar);
 }
