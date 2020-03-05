c304ee0086d3ba6dbf767406a2824241d5946b2a
lancewburklin <1677@holbertonschool.com>
2020-03-05 18:08:19 +0000

    Checked for 0 entry

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,6 +15,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	unsigned int i;
 	char *ar;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
 	ar = malloc(nmemb * size);
 	if (ar == NULL)
 		return (NULL);
