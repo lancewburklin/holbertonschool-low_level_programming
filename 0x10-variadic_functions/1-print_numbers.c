d524f24beb7a1c0072be7f39c47a7f3e7849d777
lancewburklin <1677@holbertonschool.com>
2020-03-13 15:41:39 +0000

    Prints a list of numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Prints a list of numbers
+ * @separator: The separator between numbers
+ * @n: The number of numbers
+ * @...: The numbers to print out
+ *
+ * Return: VOID
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (separator == NULL || n == 0)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
+		if (n - 1 != i)
+			printf("%s", separator);
+	}
+	printf("\n");
+}
