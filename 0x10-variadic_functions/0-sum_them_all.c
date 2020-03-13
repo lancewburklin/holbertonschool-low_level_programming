80c7708325e8ce52609caa72bb274a1ac7978ba9
lancewburklin <1677@holbertonschool.com>
2020-03-13 15:17:38 +0000

    Prints the sum of all numbers inputed

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - Finds the sum of all numbers
+ * @n: The amount of numbers to add
+ * @...: The numbers to add
+ *
+ * Return: The sum
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i, sum = 0;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
+	va_end(args);
+	return (sum);
+}
