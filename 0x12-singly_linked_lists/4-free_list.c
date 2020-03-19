2f46e833f9435316be640124e93f2d74a6201f07
lancewburklin <1677@holbertonschool.com>
2020-03-19 21:38:48 +0000

    Frees the list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @head: Head of the list
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *last;
+
+	while (head != NULL)
+	{
+		last = head;
+		head = head-> next;
+		free(last->str);
+		free(last);
+	}
+}
