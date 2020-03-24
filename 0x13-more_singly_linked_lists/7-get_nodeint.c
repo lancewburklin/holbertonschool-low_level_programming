8700382e9ba3b20962c2b99d59f6d679dcf641f3
lancewburklin <1677@holbertonschool.com>
2020-03-24 17:19:18 +0000

    Fixed check

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	unsigned int count = 0;
 	listint_t *temp = head;
 
-	while (head->next != NULL)
+	while (temp != NULL)
 	{
 		if (count == index)
 			return (temp);
