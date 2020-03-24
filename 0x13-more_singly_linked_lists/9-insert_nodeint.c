348f0306c0dce01d7f1120a76bc38090127bf809
lancewburklin <1677@holbertonschool.com>
2020-03-24 21:04:23 +0000

    Fixed for impossible positions

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -34,6 +34,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = temp;
 		temp = temp->next;
 	}
+	if (idx < count)
+		return (NULL);
 	(*head)->next = new;
 	new->next = temp;
 	*head = save;
