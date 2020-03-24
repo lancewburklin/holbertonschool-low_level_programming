b27c466019281246b12ae03d7561f8ab805ca6dc
lancewburklin <1677@holbertonschool.com>
2020-03-24 21:31:45 +0000

    Fixed several issues

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	new->next = NULL;
 	if (idx == 0)
 	{
+		new->next = *head;
 		*head = new;
 		return (new);
 	}
@@ -30,12 +31,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (count == idx)
 			break;
-		count ++;
+		count++;
 		*head = temp;
 		temp = temp->next;
 	}
-	if (idx < count)
+	if (idx > count)
+	{
+		*head = save;
+		free(new);
 		return (NULL);
+	}
 	(*head)->next = new;
 	new->next = temp;
 	*head = save;
