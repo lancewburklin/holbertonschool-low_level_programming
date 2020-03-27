cc663e8b4c58e76abefc9ecd8ea47663b6f75d0d
lancewburklin <1677@holbertonschool.com>
2020-03-27 01:54:06 +0000

    Flips a bit at an index

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Sets a bit at an index
+ * @n: Number to look through
+ * @index: Index to look for
+ *
+ * Return: The new number
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask = 1;
+
+	mask = mask << index;
+	*n = *n | mask;
+
+	return (*n);
+}
