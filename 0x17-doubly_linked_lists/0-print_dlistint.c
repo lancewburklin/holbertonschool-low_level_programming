9c3d905ec529e9b58b788f2d4624b539ac879c69
lancewburklin <1677@holbertonschool.com>
2020-05-01 17:27:18 +0000

    Prints a linked list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
