b6d97f2dcddb987d91aed759e9b94ad1aca5b817
lancewburklin <1677@holbertonschool.com>
2020-05-08 22:41:30 +0000

    Frees a doubly linked list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a doubly linked list of integers
+ * @head: Head of the list
+ *
+ * Return: Returns nothing
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *freednode;
+
+	while (head->next != NULL)
+	{
+		freednode = head;
+		head = head->next;
+		free(freednode);
+	}
+	free(head);
+}
