558fb1856594995d5342fb1919f2c4e3a842a8b1
lancewburklin <1677@holbertonschool.com>
2020-05-08 22:15:15 +0000

    Added decription of function

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - Finds the length of a list
+ * @h: The head of the list
+ *
+ * Return: length of the list
+ */
+
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
