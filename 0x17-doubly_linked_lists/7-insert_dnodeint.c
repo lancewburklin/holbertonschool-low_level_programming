44af3461d1af85c975e159e034746b08d7687753
lancewburklin <1677@holbertonschool.com>
2020-05-08 23:56:09 +0000

    Inserts a node at an index

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - Adds a node at an idex
+ * @h: Head of the list
+ * @idx: Index
+ * @n: Value to store
+ *
+ * Return: new node address
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node;
+	dlistint_t *count;
+	unsigned int i = 0;
+
+	if (*h == NULL)
+	{
+		add_dnodeint_end(h, n);
+		return(*h);
+	}
+	count = *h;
+	while (i < idx - 1 && count->next != NULL)
+	{
+		count = count->next;
+		i++;
+	}
+	printf("OUt of loop\n");
+	if (idx > i)
+		return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->next = count->next;
+	new_node->prev = count;
+	count->next = new_node;
+	new_node->next->prev = new_node;
+	new_node->n = n;
+	return (new_node);
+}
